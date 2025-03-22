d438ce637a729761b6665d5d2cca46d67b305b1c
Adil G <ad.guluzade@gmail.com>
2025-03-21 21:06:20 -0400

    Update Cart.js

--- EECS4413CarShop/models/Cart.js
+++ EECS4413CarShop/models/Cart.js
@@ -2,18 +2,18 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 // This schema assumes the input of product number and product quantity value as mandatory entries
-const CartItemS = new Schema ({
+const CartItemSchema = new Schema ({
   product: {type: String, required: true},
   quantity: {type: Number, required: true, min: 1}
 });
 
 // This schema assumes the input of username/userID, however for now it is not required 
-// const cartSchemaUser = new Schema({
-//   user: { type: Schema.Types.ObjectId, ref: 'user', required: false }, 
-//   items: [CartItemS]
-// });
+const cartSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'user', required: false }, 
+  items: [CartItemSchema]
+});
 
-const Cart = mongoose.model('Cart', CartItemS);
+const Cart = mongoose.model('Cart', cartSchema);
 export default Cart;
 
   
