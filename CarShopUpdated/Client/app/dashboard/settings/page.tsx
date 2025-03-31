81b65c9853b02d2b322fff708b2495a70c112994
amiradn <amiradn4@my.yorku.ca>
2025-03-31 19:40:00 -0400

    Delete CarShopUpdated/Client directory

--- CarShopUpdated/Client/app/dashboard/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-import SideNav from "@/app/componets/dashboard/sidenav";
-import { redirect } from "next/navigation";
-
-export default function SettingsPage() {
-  
-  redirect("/dashboard/settings/profile"); // Instantly redirects
-  return null;
-}
