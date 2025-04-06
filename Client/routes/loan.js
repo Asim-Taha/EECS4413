58676a61dcfbe243111a493a8e4a82009ab33f40
Asim-Taha <77361076+Asim-Taha@users.noreply.github.com>
2025-04-06 00:06:10 -0400

    Delete Client directory

--- Client/routes/loan.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { calculateLoan } from '../controllers/loanController.js';
-
-const router = express.Router();
-
-router.post('/calculate', calculateLoan);
-
-export default router;
